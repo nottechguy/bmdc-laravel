d6ed7f3f1de72cd3ba5d65f786435716d6fe2b6f
Luis Antonio <la.mendoza.1618@gmail.com>
2026-01-04 15:26:31 -0600

    refactor: adding activated attribute

--- resources/views/components/navigation/drawer/drawer-item.blade.php
+++ resources/views/components/navigation/drawer/drawer-item.blade.php
@@ -1,11 +1,25 @@
-<a class="{{ $classNames() }}" href="{{ $href ?? '#' }}" {{ $attributes->merge([]) }}>
+<a
+    @class([
+        $classList['container'],
+        $classList['activated'] => $activated,
+        $classList['disabled'] => $disabled,
+    ])
+    href="{{ $disabled ? null : ($href ?? '#') }}"
+    @if($activated) aria-current="page" @endif
+    @if($disabled) aria-disabled="true" tabindex="-1" @endif
+    {{ $attributes }}
+>
     @if ($icon !== null)
         <x-bmdc-icon class="{{ $classList['icon'] }}" name="{{ $icon }}" />
     @endif
 
-    {{ $text }}
-    
+    <span class="{{ $classList['text'] }}">
+        {{ $text }}
+    </span>
+
     @if ($badge !== null)
-        <span class="{{ $classList['supporting_text'] }}">{{ $badge }}</span>
+        <span class="{{ $classList['supporting_text'] }}">
+            {{ $badge }}
+        </span>
     @endif
 </a>
