b39c98545f0f1af3bd9b90ebcdd4f24b9278ac6b
Luis Antonio <la.mendoza.1618@gmail.com>
2026-03-07 10:37:11 -0600

    refactor: add end alignment class

--- src/Components/navigation/BMDCMenu.php
+++ src/Components/navigation/BMDCMenu.php
@@ -6,18 +6,22 @@ use Illuminate\View\Component;
 
 class BMDCMenu extends Component {
     public string $type;
+    public string $align;
 
     public array $classList = [
         'container'         => 'c_menu',
         'exposed'           => 'c_menu--exposed',
+        'end_aligned'       => 'c_menu--end-aligned',
         'surface'           => 'c_menu-surface',
         'surface_open'      => 'c_menu-surface--open'
     ];
 
     public function __construct(
-        string $type = 'dropdown'
+        string $type = 'dropdown',
+        string $align = 'left'
         ) {
         $this->type  = $type;
+        $this->align = $align;
     }
 
     public function classNames() {
@@ -25,6 +29,10 @@ class BMDCMenu extends Component {
         
         if ($this->type == 'exposed') {
             $classes[] = $this->classList['exposed'];
+        }
+
+        if ($this->align == 'right') {
+            $classes[] = $this->classList['end_aligned'];
         }
         
         return implode(' ', $classes);
