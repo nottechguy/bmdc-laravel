76cb40d603c245367cf02c61969d50cd19aadde6
Luis Antonio <la.mendoza.1618@gmail.com>
2026-05-24 19:58:47 -0600

    refactor: add slot for svg icon

--- resources/views/components/action/fab.blade.php
+++ resources/views/components/action/fab.blade.php
@@ -1,6 +1,17 @@
+@php
+
+$_icon = $icon;
+$_hasIconSlot = isset($mIcon) && $mIcon->isNotEmpty();
+
+@endphp
+
 <button {{ $attributes->merge(["class" => $classNames(), "type" => "button"]) }}>
-    @if ($icon !== "")
-        <x-bmdc-icon class="{{ $classList['icon'] }}" name="{{ $icon }}" />
+    @if ($_icon !== NULL || $_hasIconSlot)
+        @if ($_icon !== NULL && !$_hasIconSlot)
+            <x-bmdc-icon class="{{ $classList['icon'] }}" name="{{ $icon }}" />
+        @elseif ($_hasIconSlot && !$_icon)
+            <span class="{{ $classList['icon'] }}">{{ $mIcon }}</span>
+        @endif
     @endif
     @if ($label !== "")
         <span class="{{ $classList['label'] }}">{{ $label }}</span>
